fe7a6aaa437fc8b634f42188e0af25085e3ab4b7
Mathieu Voland <mathieu.voland@gmail.com>
2020-08-10 11:21:58 +0200

    Can signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  # See https://www.sitepoint.com/enumerated-types-with-activerecord-and-postgresql/
   enum status: %i[work_in_progress need_help can_help]
 
   def name
--- app/services/discord_bot.rb
+++ app/services/discord_bot.rb
@@ -2,7 +2,7 @@ class DiscordBot
   @@discord_bot = nil
 
   def self.run
-    return 'Déjà réveillé :)' unless @@discord_bot.nil?
+    return false unless @@discord_bot.nil?
 
     @@discord_bot = Discordrb::Commands::CommandBot.new token: ENV['DISCORD_BOT_TOKEN'],
                                                         client_id: ENV['DISCORD_CLIENT_ID'],
@@ -10,20 +10,33 @@ class DiscordBot
                                                         advanced_functionality: true
     bot = @@discord_bot
 
-    # bot.message(with_text: 'Hi') do |event|
-    #   event.respond 'Hey!'
-    #   puts "Said HEY to #{event.user.username}"
-    # end
+    bot.command :sign_up do |event|
+      user = User.new
+      user.discord_id = event.user.id
+      user.discord_username = event.user.username
+      user.work_in_progress!
+      if user.save
+        event.respond "Merci, tu es bien inscrit #{event.user.username}"
+      else
+        event.respond "Il y a eu un problème avec l'inscription #{event.user.username}"
+      end
+      event.respond "Plus d'information sur <https://the-hacking-bot.herokuapp.com>"
+    end
 
-    bot.command :hi do |event|
-      event.respond "Hi #{event.user.username}"
-      puts "Said Hi to #{event.user.username}"
-      puts event.methods
+    bot.command :need_help do |event|
+      user = User.find_by(discord_id: event.user.id)
+      puts user
+      if user
+        user.need_help!
+        event.respond "OK #{user.name}"
+      else
+        event.respond "Utilisateur #{event.user.username} non trouvé, est-tu bien inscrit avec '$sign_up' ?"
+      end
     end
 
     at_exit { bot.stop }
     bot.run :async
 
-    'On a réveillé le bot, merci'
+    true
   end
 end
