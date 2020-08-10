878e844271489643ed6df9cca9263ae3a3a96c9a
Mathieu Voland <mathieu.voland@gmail.com>
2020-08-10 12:06:42 +0200

    Update Documentation

--- README.md
+++ README.md
@@ -8,3 +8,29 @@ Un tutoriel sur la création d'un bot Discord avec la [gem discordrb](https://gi
 
 Le site est mis en ligne sur heroku : [the-hacking-bot.herokuapp.com](https://the-hacking-bot.herokuapp.com/), une petite documentation est disponible sur la page d'accueil.
 
+## Fonctionnement
+
+La première étape est de s'inscrire, pour cela il suffit de taper la commande `$sign_up` en envoyant un message à The Hacking Bot.
+
+Ensuite le principe est de pouvoir définir trois statuts :
+
+ * `$need_help` : j'ai besoin d'aide
+ * `$can_help` : je peux aider
+ * `$work_in_progress` : je bosse, laisse moi tranquile
+
+Pour l'instant cela met seulement à jours la page [the-hacking-bot.herokuapp.com/users](https://the-hacking-bot.herokuapp.com/users) du site.
+
+Pour avoir la liste des commandes disponibles : `$help`
+
+## Pour la suite
+
+L'idée est ensuite de laisser le bot mettre directement en relation les personnes, éventuellement pouvoir indiquer sur quel point on a besoin d'aide, et proposer une vrai page personnelle par utilisateur, etc.
+
+Je cherche d'abord des retours sur :
+
+ * Est-ce que ça peut être utile ?
+ * Qu'est ce qu'il faudrait améliorer ?
+ * Qu'est ce qui peut poser problème (le fait d'être affiché sur une page publique par exemple ?)
+ * Bon le front est horrible : 1. je suis pas très à l'aise, 2. je n'ai pas commencé à y réfléchir
+
+Merci pour tout retour !
--- app/services/discord_bot.rb
+++ app/services/discord_bot.rb
@@ -23,15 +23,63 @@ class DiscordBot
       event.respond "Plus d'information sur <https://the-hacking-bot.herokuapp.com>"
     end
 
+    bot.command :sign_out do |event|
+      user = User.find_by(discord_id: event.user.id)
+      if user
+        if user.destroy
+          event.respond "Désolé de te voir partir #{event.user.username}"
+        else
+          event.respond 'Il y a eu un problème avec la désinscription, merci de nous faire un retour'
+        end
+      else
+        event.respond "Utilisateur #{event.user.username} non trouvé, est-tu bien inscrit avec '$sign_up' ?"
+      end
+      event.respond "Plus d'information sur <https://the-hacking-bot.herokuapp.com>"
+    end
+
     bot.command :need_help do |event|
       user = User.find_by(discord_id: event.user.id)
-      puts user
       if user
         user.need_help!
         event.respond "OK #{user.name}"
       else
         event.respond "Utilisateur #{event.user.username} non trouvé, est-tu bien inscrit avec '$sign_up' ?"
       end
+      event.respond "Plus d'information sur <https://the-hacking-bot.herokuapp.com>"
+    end
+
+    bot.command :can_help do |event|
+      user = User.find_by(discord_id: event.user.id)
+      if user
+        user.can_help!
+        event.respond "OK #{user.name}"
+      else
+        event.respond "Utilisateur #{event.user.username} non trouvé, est-tu bien inscrit avec '$sign_up' ?"
+      end
+      event.respond "Plus d'information sur <https://the-hacking-bot.herokuapp.com>"
+    end
+
+    bot.command :work_in_progress do |event|
+      user = User.find_by(discord_id: event.user.id)
+      if user
+        user.work_in_progress!
+        event.respond "OK #{user.name}"
+      else
+        event.respond "Utilisateur #{event.user.username} non trouvé, est-tu bien inscrit avec '$sign_up' ?"
+      end
+      event.respond "Plus d'information sur <https://the-hacking-bot.herokuapp.com>"
+    end
+
+    bot.command :help do |event|
+      event.respond "Liste des commandes (The Hacking Bot):
+------------------------------------------------------------------------
+`$help . . . . . . .` > ce message
+`$sign_up  . . . . .` > je veux m'inscrire
+`$sign_out . . . . .` > je veux me désinscrire
+`$can_help . . . . .` > modifier mon statut, je peux aider quelqu'un
+`$need_help  . . . .` > modifier mon statut, j'ai besoin d'aide
+`$work_in_progress .` > modifier mon statut, je ne veux pas être dérangé"
+      event.respond "Plus d'information sur <https://the-hacking-bot.herokuapp.com>"
     end
 
     at_exit { bot.stop }
